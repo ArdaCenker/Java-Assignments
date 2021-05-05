cffd805b2c194e915ebe2cdbfb57f408aa078ba5
Arda Cenker Karagöz <ardacenkerkaragoz@gmail.com>
2021-05-05 10:15:09 +0300

    Add project files

--- 4thDay_2ndAssignment/src/Abstract/CustomerCheckService.java
+++
@@ -1,9 +0,0 @@
-package Abstract;
-
-import java.rmi.RemoteException;
-
-import Entities.Customer;
-
-public interface CustomerCheckService {
-	boolean CheckIfRealPerson(Customer customer) throws RemoteException;
-}
